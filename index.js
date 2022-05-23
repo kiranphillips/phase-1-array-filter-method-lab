6bd39a659c54a5ef063d3790978cadee08b2d9a0
Kiran Phillips <Kiranphillips61@gmail.com>
2022-05-23 17:16:22 -0600

    commit tree 821669ad8355a5ba0e3b20b3b9d4e2ffbb09a330

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter (driver => driver.toLowerCase() === name.toLowerCase())
+  }
+  function fuzzyMatch(drivers, letters){
+      return drivers.filter(driver => driver.indexOf(letters) === 0)
+    }
+  function matchName(drivers, match){
+      return drivers.filter(driver => driver.name === match)
+  }
